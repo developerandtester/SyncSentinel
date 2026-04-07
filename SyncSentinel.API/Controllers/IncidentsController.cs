e79a29354c9fce93841b4a4c9e9fea2610ed4b2d
Prathamesh Vyas <vyasprathmesh@gmail.com>
2026-04-07 16:38:39 +0530

    Added base to Program.cs

---
+++ SyncSentinel.API/Controllers/IncidentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SyncSentinel.API.Controllers
+{
+    public class IncidentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
