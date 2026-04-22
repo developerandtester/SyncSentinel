2155321510f00520b3e05a1d24e56adb14148af2
Prathamesh Vyas <vyasprathmesh@gmail.com>
2026-04-22 17:24:51 +0530

    Added DTOs for Syncing Jobs

---
+++ SyncSentinel.Application/DTOs/SyncJobs/CreateSyncJobRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using SyncSentinel.Domain.Enums;
+
+namespace SyncSentinel.Application.DTOs.SyncJobs;
+
+public class CreateSyncJobRequest
+{
+    [Required]
+    public Guid? IncidentId { get; set; }
+
+    [Required]
+    public JobType JobType { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string SourceSystem { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(100)]
+    public string TargetSystem { get; set; } = string.Empty;
+
+    [Required]
+    public string Payload { get; set; } = string.Empty;
+
+    [Range(0, 20)]
+    public int MaxRetries { get; set; } = 3;
+}
